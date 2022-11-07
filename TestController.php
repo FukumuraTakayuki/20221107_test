6606185b0660c1eed5811d23160e3e6f86f53b72
FukumuraTakayuki <fmty@MacBook-Air29.local>
2022-11-07 17:56:07 +0900

    first-commit

--- TestController.php
+++ TestController.php
@@ -1,13 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 
-use Illuminate\Http\Request;
+Route::get('/building', [TestController::class, 'index']);
 
-class TestController extends Controller
-{
-  public function index()
-  {
-    return "建物です";
-  }
-}
+Route::get('/building/{room}', function ($room,) {
+  return '部屋番号は' . $room . 'です';
+});
